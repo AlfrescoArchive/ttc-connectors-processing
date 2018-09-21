81383e0c5dd371e00efbc3f6d41d89e7108b66db
Elias Ricken de Medeiros <erdemedeiros@users.noreply.github.com>
2018-09-21 09:55:12 +0200

    Update dependencies after pom refactoring (#5)
    - Update groupIds
    
    - Update package names

--- src/main/java/org/activiti/cloud/connectors/processing/connector/TweetAnalyzerConnector.java
+++ src/main/java/org/activiti/cloud/connectors/processing/connector/TweetAnalyzerConnector.java
@@ -1,11 +1,14 @@
 package org.activiti.cloud.connectors.processing.connector;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.activiti.cloud.api.process.model.IntegrationRequest;
+import org.activiti.cloud.api.process.model.IntegrationResult;
 import org.activiti.cloud.connectors.processing.analyzer.NLP;
 import org.activiti.cloud.connectors.starter.channels.IntegrationResultSender;
 import org.activiti.cloud.connectors.starter.configuration.ConnectorProperties;
 import org.activiti.cloud.connectors.starter.model.IntegrationResultBuilder;
-import org.activiti.runtime.api.model.IntegrationRequest;
-import org.activiti.runtime.api.model.IntegrationResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +17,6 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static net.logstash.logback.marker.Markers.append;
 
@@ -37,7 +37,7 @@ public class TweetAnalyzerConnector {
     }
 
     @StreamListener(value = ProcessingConnectorChannels.TWITTER_ANALYZER_CONSUMER)
-    public void analyzeEnglishTweet(IntegrationRequest event) throws InterruptedException {
+    public void analyzeEnglishTweet(IntegrationRequest event) {
 
         String tweet = String.valueOf(event.getIntegrationContext().getInBoundVariables().get("text"));
 
